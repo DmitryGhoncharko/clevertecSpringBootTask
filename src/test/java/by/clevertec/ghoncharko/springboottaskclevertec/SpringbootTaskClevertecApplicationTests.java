9817a9a001831f09a8c08d41218972ae6d338f48
dangeonMaster <dafae722@gmail.com>
2023-06-19 15:01:09 +0300

    init proj

---
+++ src/test/java/by/clevertec/ghoncharko/springboottaskclevertec/SpringbootTaskClevertecApplicationTests.java
@@ -0,0 +1,13 @@
+package by.clevertec.ghoncharko.springboottaskclevertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTaskClevertecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
